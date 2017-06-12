5310b4fbe6f9850bd14027ea507c7a8fd9325729
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2017-06-12 13:52:52 -0400

    Fairfax Corner 1.0  /site.js

--- site.js
+++ site.js
@@ -1,6 +1,6 @@
 var site_json = {
                     "mall_name" : "Fairfax Corner",
-                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/580f9dab6e6f643917000000/image/png/1479159781000/Fairfax Corner Logo White copy.png",
+                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/580f9dab6e6f643917000000/image/jpeg/1497286652000/FC.jpg",
                     "time_zone" : "US/Eastern",
                     "newsletter_form_url" : "http://mobilefringe.createsend.com/t/d/s/bkhoy/?callback=?",
                     "newsletter_email_field" : "cm-bkhoy-bkhoy"
